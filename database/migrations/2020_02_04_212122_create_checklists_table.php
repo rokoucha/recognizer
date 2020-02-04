3599f625731bb50d4d19bc63a35434bd835bf9b3
Rokoucha <rokoucha@rokoucha.net>
2020-02-04 22:08:02 +0900

    feat: Add migrations

---
+++ database/migrations/2020_02_04_212122_create_checklists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChecklistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 255);
+            $table->bigInteger('user')->unsigned();
+            $table->foreign('user')->references('id')->on('users');
+            $table->json('checks');
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checklists');
+    }
+}
---
+++ database/migrations/2020_02_04_212722_create_attachments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->timestamps();
+            $table->bigInteger('user')->unsigned();
+            $table->foreign('user')->references('id')->on('users');
+            $table->bigInteger('checklist')->unsigned();
+            $table->foreign('checklist')->references('id')->on('checklists');
+            $table->string('path', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
