d7167332b0ab69947b4a6d1ff76b152de9660473
Rokoucha <rokoucha@rokoucha.net>
2020-02-04 22:08:23 +0900

    feat: Add database seeders

---
+++ database/seeds/AttachmentsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Attachment;
+use Illuminate\Database\Seeder;
+
+class AttachmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $attachments = [
+            [
+                'name' => 'pr.png',
+                'user' => 1,
+                'checklist' => 1,
+                'path' => '/upload/pr.png'
+            ],
+            [
+                'name' => 'good.png',
+                'user' => 1,
+                'checklist' => 1,
+                'path' => '/upload/good-pr.png'
+            ],
+        ];
+
+        foreach($attachments as $attachment) {
+            Attachment::create($attachment);
+        }
+    }
+}
