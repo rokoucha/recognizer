f14aed89d14e058f17f8fafe088cfada71ed608a
Rokoucha <rokoucha@rokoucha.net>
2020-02-28 17:57:21 +0900

    feat: Add beautiful index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,21 @@
     <div class="w-full text-center mb-2 text-xl text-teal-300">
         Recognizer - Support your recognition.
     </div>
-    <a class="block mt-4" href="/checklist/create">
-        <div class="md:max-w-full md:flex bg-gray-500 justify-center">
+    <p class="w-full break-words mb-2">
+        Recognizer is simple checklist application.
+        "Pointing and calling" makes strongly fewer mistakes.
+        This application will support "Pointing and calling" with a checklist.
+
+        Recognizer provides ordered or unordered checklists.
+        Of course, nesting is also possible.
+        Also, you can attach pictures and more information to the checklist.
+    </p>
+    <img class="mb-2" alt="Checklist" src="{{ asset('images/checklist.png') }}">
+    <img class="mb-2" alt="Edit" src="{{ asset('images/editor.png') }}">
+    <a class="block mt-4" href="/checklist">
+        <div class="max-w-full flex bg-teal-300 justify-center">
             <div class="mb-4 mt-2 ml-2 mr-2 font-bold text-xl text-black">
-                Create
+                View Checklists
             </div>
         </div>
     </a>
