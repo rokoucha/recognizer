9687329d3eb781e5694ac6b9460e2c0a101eca86
Rokoucha <git@rokoucha.net>
2020-03-05 01:03:36 +0900

    fix: Use Request instead of Input facade

--- app/Http/Controllers/Api/ChecklistsController.php
+++ app/Http/Controllers/Api/ChecklistsController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Checklist;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
 
 class ChecklistsController extends Controller
 {
-    public function index()
+    public function index(request $request)
     {
-        if (empty(Input::get('name'))) return [];
+        $name = $request->input('name');
+        if (empty($name)) return [];
 
-        $name = Input::get('name');
         return Checklist::select('id', 'name')
             ->where('name', 'LIKE', "%$name%")
             ->limit(10)->get();
